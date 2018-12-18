cab00d653126c8abb1b4cd2a372844d54adb75ab
assetozen <assetozen@yahoo.com>
2018-12-18 16:31:54 +0100

    Second Commit - Java doc comment added to the controllers 	- home 	- contactus

--- WebAppViaSpringBoot/src/main/java/com/assegd/demo/ContactUsController.java
+++ WebAppViaSpringBoot/src/main/java/com/assegd/demo/ContactUsController.java
@@ -3,6 +3,11 @@ package com.assegd.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * This Controller will trigger the contactus.jsp page when called on the browser with http://localhost:8080/contactus
+ * @author a.asfaw
+ *
+ */
 @Controller
 public class ContactUsController 
 {
--- WebAppViaSpringBoot/src/main/java/com/assegd/demo/HomeController.java
+++ WebAppViaSpringBoot/src/main/java/com/assegd/demo/HomeController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * this controller will trigger the home.jsp when http://localhost:8080/home?aid=11&aname=Abebe
+ * @author a.asfaw
+ *
+ */
 @Controller
 public class HomeController {
 	
